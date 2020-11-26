8f87039b0be6d2b7130524b00cd43db895ac4dba
曾啟豪 <2996689717@qq.com>
2020-11-26 16:29:52 +0800

    2048

---
+++ js/showanimation2048.js
@@ -0,0 +1,26 @@
+function showNumberWithAnimation(i,j,randNumber){
+	var numberCell=$('#number-cell-'+i+'-'+j);
+	
+	numberCell.css('background-color',getNumberBackgoundColor(randNumber));
+	numberCell.css('color',getNumberColor(randNumber));
+	numberCell.text(randNumber);
+	
+	numberCell.animate({
+		width:cellSideLength,
+		height:cellSideLength,
+		top:getPosTop(i,j),
+		left:getPosLeft(i,j)
+	},50)
+}
+
+function showMoveAnimation(fromx, fromy, tox, toy){
+	var numberCell=$('#number-cell-'+fromx+'-'+fromy); 
+	numberCell.animate({
+		top:getPosTop(tox,toy),
+		left:getPosLeft(tox,toy)
+	},100)
+}
+
+function updateScore(score){   //更新分数
+	$('#score').text(score);
+}
